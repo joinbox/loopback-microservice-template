afd1c02fa95882ef1c31c98ae709f5d0b5036b5e
rcky <ruefe@rcky.ch>
2017-12-12 16:59:05 +0100

    chore: add component integration

---
+++ app/boot/9999-run-migrations.js
@@ -0,0 +1,10 @@
+module.exports = async function(app, next) {
+    const migration = app.get('loopback-component-jb-migration');
+    const logger = app.get('microservice-logger');
+    try {
+        await migration.run({ app, logger });
+    } catch (err) {
+        return next(err);
+    }
+    next();
+};
