d0135bbb25514f781b81ef3896c112a345f29652
rcky <ruefe@rcky.ch>
2018-07-25 18:02:00 +0200

    chore: removes dummy project

--- app/config/defaultBootOptions.js
+++ app/config/defaultBootOptions.js
@@ -4,7 +4,7 @@ const dsRootDir = appConfigRootDir;
 const modelsRootDir = appConfigRootDir;
 const middlewareRootDir = appConfigRootDir;
 const componentRootDir = appConfigRootDir;
-const bootDirs = [`${appRootDir}/boot`, 'loopback-dummy-project/boot'];
+const bootDirs = [`${appRootDir}/boot`];
 
 module.exports = {
     appRootDir,
