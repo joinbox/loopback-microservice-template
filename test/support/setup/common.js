89b66c51dd1acf96ac66d47249c6a164e02fbb08
rcky <ruefe@rcky.ch>
2018-07-30 14:40:28 +0200

    fix: move require out of method

--- test/support/setup/common.js
+++ test/support/setup/common.js
@@ -5,11 +5,12 @@ const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const Microservice = require('@joinbox/loopback-microservice');
 
+const boot = require('./testBootOptions');
+
 const testLib = require('../lib');
 
 chai.use(chaiAsPromised);
 
 before(async function() {
-    const boot = require('./testBootOptions');
     this.service = await Microservice.boot({ boot });
 });
